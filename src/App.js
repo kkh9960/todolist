21978625ac2202dfeb38c6bf22d6c4f5d5ce7a6b
Kwu Hyen Kwak <kkjh9960@gmail.com>
2022-10-06 11:57:03 +0900

    제출

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      value: "react를 배워 봅시다.",
+    },
+    {
+      value: "useState를 배워 봅시다.",
+    },
+  ]);
+
+  const initialState = {
+    value: "",
+  };
+
+  const [todo, setTodo] = useState(initialState);
+  const onChangeHandler = (event) => {
+    const inputValue = event.target.value;
+    setTodo({ ...todo, value: inputValue });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (todo.value.trim() === "") return;
+    setTodoList([...todoList, { ...todo }]);
+    setTodo(initialState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-con">
+      <form className="add-form">
+        <div>
+          <input
+            type="text"
+            name="content"
+            className="add-input"
+            value={todo.value}
+            onChange={onChangeHandler}
+          />
+          <button className="add-button" onClick={onSubmitHandler}>
+            추가하기
+          </button>
+        </div>
+      </form>
+      <h2>Todo List</h2>
+      <div className="square">
+        {todoList.map((todo) => {
+          return <div>{todo.value}</div>;
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
